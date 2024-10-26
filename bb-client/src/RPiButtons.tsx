3749b63a6418c4fa44db95808f3d1380642ae604
Ksenia Mordovets <kmordovets@gmail.com>
2024-10-26 22:55:23 +0200

    Refactor RPiButtons component to use mock data for fetching RPi data

--- bb-client/src/RPiButtons.tsx
+++ bb-client/src/RPiButtons.tsx
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { fetchAllRPis } from './services/RPiDBService'; // Import the API service
 import { RPi } from './types/Rpi';
+import { mockData } from './data/mockData';
 
 const RPiButtons = () => {
     const [rpis, setRpis] = useState<RPi[]>([]);
 
     useEffect(() => {
         const getData = async () => {
-            try {
-                const data = await fetchAllRPis();
-                console.log('Fetched data:', data); // Log the fetched data
-                setRpis(Array.isArray(data) ? data : []); // Ensure data is an array
-            } catch (error) {
-                console.error('Error fetching RPi data:', error);
-            }
+            setRpis(mockData);
+            //try {
+            //    const data = await fetchAllRPis();
+            //    console.log('Fetched data:', data); // Log the fetched data
+            //    setRpis(Array.isArray(data) ? data : []); // Ensure data is an array
+            //} catch (error) {
+            //    console.error('Error fetching RPi data:', error);
+            //}
         };
 
         getData();
     }, []);
 
     return (
-        <div>
+        <div className='bg-red-600'>
             {rpis.map(rpi => (
                 <button
                     key={rpi.mac_address}
---
+++ bb-client/src/data/mockData.ts
@@ -0,0 +1,17 @@
+export const mockData = [
+    {
+        mac_address: '00:00:00:00:00:00',
+        subdomain: 'rpi1',
+        last_updated: '2021-09-01T12:00:00Z',
+    },
+    {
+        mac_address: '00:00:00:00:00:01',
+        subdomain: 'rpi2',
+        last_updated: '2021-09-01T12:00:00Z',
+    },
+    {
+        mac_address: '00:00:00:00:00:02',
+        subdomain: 'rpi3',
+        last_updated: '2021-09-01T12:00:00Z',
+    },
+];
