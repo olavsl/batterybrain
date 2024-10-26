f8c4395ff08ed0327204118643b65ed1c366cf95
Ksenia Mordovets <kmordovets@gmail.com>
2024-10-26 13:22:18 +0200

    Refactor App component to use RPiButtons instead of ExternalFileButton

---
+++ bb-client/src/RPiButtons.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const RPiButtons = () => {
+    const [rpis, setRpis] = useState([]);
+
+    useEffect(() => {
+        fetch('http://databoysxko.codexenmo.no:8081/api/rpis')
+            .then(response => response.json())
+            .then(data => setRpis(data))
+            .catch(error => console.error('Error fetching RPi data:', error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Connect to Raspberry Pis</h2>
+            {rpis.map(rpi => (
+                <button
+                    key={rpi.id}
+                    onClick={() => window.location.href = `http://${rpi.subdomain}`}
+                >
+                    Connect to {rpi.id}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+export default RPiButtons;
