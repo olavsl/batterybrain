52c4b5fe894df5e44caab04d76eb5d4052248ace
Ksenia Mordovets <kmordovets@gmail.com>
2024-10-26 22:04:12 +0200

    Refactor RPiDBService to use relative API endpoint for fetching RPi data

--- bb-client/src/services/RPiDBService.ts
+++ bb-client/src/services/RPiDBService.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 // Set the base URL of your API
 const api = axios.create({
-    baseURL: 'http://51.120.13.32:5000', // Update this to the correct server IP and port if needed
+    baseURL: '/api', // Update this to the correct server IP and port if needed
 });
 
 // Function to fetch all Raspberry Pi entries
 export const fetchAllRPis = async () => {
     try {
-        const response = await api.get('/api/rpis');
+        const response = await api.get('/rpis');
         return response.data;
     } catch (error) {
         console.error('Error fetching Raspberry Pi entries:', error);
